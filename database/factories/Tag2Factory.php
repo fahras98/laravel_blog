cd461e9affa09096bb581055bdd09d2ba25a7caa
fihras98 <yousfts10@gmail.com>
2021-07-27 16:45:40 +0100

    all files

---
+++ database/factories/Tag2Factory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\tag2;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class Tag2Factory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = tag2::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
